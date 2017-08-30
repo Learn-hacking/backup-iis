eea1a59b6aebaa9b5e9d0b41df0f91d579da731a
androidlover5842 <androidlover5842@gmail.com>
2017-08-30 17:09:10 +0530

    something new

---
+++ special.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "this is script name : $0"
+echo
+echo "this is the 1st argu: $1"
+echo
+echo "this is the 2nd argu: $2"
+echo
+echo "this is the 3rd argu: $3"
+echo
+echo \$ "$$ this is the PID of the script"
+echo
+echo \# "$@ all aruguments"
+echo \$ "$* "
+echo 
+echo "$#"
+
+echo "$? echo 1 = failed need some help 0= passed :)"
+
